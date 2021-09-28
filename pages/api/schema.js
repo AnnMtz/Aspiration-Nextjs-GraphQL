20554b8084ffe500599011f99f03170465b69c45
Ana Martínez <49168661+AnnMtz@users.noreply.github.com>
2021-09-28 13:50:07 -0500

    Update schema.js

--- pages/api/schema.js
+++ pages/api/schema.js
@@ -1,22 +1,3 @@
-// export const query = `
-// { 
-//     topic(name:"react") {
-//         id
-//         name
-//         relatedTopics(first: 10) {
-//             id
-//             name
-//             stargazerCount
-//             viewerHasStarred
-//             relatedTopics(first: 10) {
-//                 id
-//                 name
-//                 stargazerCount
-//                 viewerHasStarred
-//             }
-//         }
-//     }
-// }`;
 import { gql } from '@apollo/client';
 
 export const QUERY = gql `
